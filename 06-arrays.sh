e60c204b22749dcaf42b00b57ede8a8d6923a385
unknown <chandrasekhar.maroju@gmail.com>
2024-06-07 09:12:13 +0530

    Updated 6th script-arguments,variables

---
+++ 06-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Mango")
+ echo "First Value is : ${FRUITS[0]}"
+ echo "Second Value is : ${FRUITS[1]}"
+ echo "Third Value is : ${FRUITS[2]}"
+ 
+ echo "All values in array : ${FRUITS[@]}"

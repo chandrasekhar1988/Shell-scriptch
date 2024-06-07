4a929fddd73c1b899b960a0fec1f88d58289eec9
unknown <chandrasekhar.maroju@gmail.com>
2024-06-07 08:46:20 +0530

    Updated 4th script-arguments,variables

---
+++ 04-arguments-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON3=$1
+PERSON4=$2
+
+echo "$PERSON3: Hello $PERSON4,Good Morning"
+echo "$PERSON4: Hi $PERSON3,Very Good Morning"
+echo "$PERSON3: How are you $PERSON4?"
+echo "$PERSON4: Iam Fine! Thank you $PERSON3"

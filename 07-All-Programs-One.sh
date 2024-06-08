70d91922e28897ec6212a96af6ead96852482ce2
unknown <chandrasekhar.maroju@gmail.com>
2024-06-08 07:39:10 +0530

    added 7th script-practice

--- 07-All-Programs-One.sh
+++ 07-All-Programs-One.sh
@@ -1,15 +1,18 @@
 #!/bin/bash    
 
 echo "1.This is My practice program to Check all commands" 
+echo "---------------------------------"
 
 echo -e "\n2.Enter your Name : "
 read Name
 
+echo "---------------------------------"
 echo "3.I entered my name in input as:" 
 echo $Name
 
+echo "---------------------------------"
 #Variables 
-
+echo "4.Variables concept:"
 country=India  #Assign the value directly
-echo $country
+echo "\nMy country name:" $country
 

682aad95fe2c37c4ecdb3b1feef0f891b8dee61b
unknown <chandrasekhar.maroju@gmail.com>
2024-06-08 10:36:00 +0530

    added 7th script-practice

--- 10-maths-operations.sh
+++ 10-maths-operations.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-
+#Addition/Subtract two numbers using the +/- 
+#operator between defined variables and enclose them with $().
 num1=10
 num2=20
 num3=30
 sum=$(($num1+$num2+$num3))
-//sum=$((10+20+30))
 echo "The Sum is: $sum"
 
-dif=$(($num3-$num2))
+dif=$(($num3-$num1))
 echo "The difference is:$dif"
+
+num1=30
+num2=5
+div=$(($num3/$num1))
+
+echo "The division is: $div

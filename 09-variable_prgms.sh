a192135288c352aa5ed51ea97c401ebf400d1448
unknown <chandrasekhar.maroju@gmail.com>
2024-06-08 14:41:20 +0530

    added 7th script-practice

---
+++ 09-variable_prgms.sh
@@ -0,0 +1,21 @@
+#!/bin/bash    
+#1.Assign the value directly
+country=India  
+echo -e "\nMy country name:" $country
+
+#2.Concatenating Multiple Variables
+# Declaration of variables
+name='My name is Anshu.' 
+age='My age is 8.'
+info="${name} ${age}"
+echo "Result: $info"
+
+#3.Passing Values to Variables as Command Line Arguments
+aspirant=$1
+score=$2
+echo "aspirant_name is --> $aspirant. scored--> $score"
+# we have to give run command with values sh 09-variable_prgms.sh chandu 70
+
+
+
+

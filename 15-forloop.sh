e3db30896a13f53712a0aedf71599e01042ab502
unknown <chandrasekhar.maroju@gmail.com>
2024-06-08 12:24:17 +0530

    added 7th script-practice

--- 15-forloop.sh
+++ 15-forloop.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #For Loop : Print Even Numbers From 1 to 10 
-for (( i=1; i<=10; i++ ))
-do
-#if [ $((i%2)) == 0 ]
-#then
-echo $i
-#fi
+# for (( i=1; i<=10; i++ ))
+# do
+# #if [ $((i%2)) == 0 ]
+# #then
+# echo $i
+# #fi
+# done
+
+arr=("mango" "grape" "apple" "cherry" "orange")
+for item in "${arr[@]}"; do
+echo $item
 done

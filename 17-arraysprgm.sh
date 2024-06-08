7ef5f2f41c09e866ba931d028fbd5cce085f9d17
unknown <chandrasekhar.maroju@gmail.com>
2024-06-08 13:42:18 +0530

    added 7th script-practice

--- 17-arraysprgm.sh
+++ 17-arraysprgm.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#we declare the array along with the values.
 arr=(24 27 84 11 99)
 
+
+#we only assign value to a specific index.
+arr[5]=78
+# To print all elements of the array
 echo "Given array: ${arr[*]}"
+echo “Printing Array again${arr[@]}”
+
+# To print the first element
+echo “The first element:”
+echo “${arr[0]}”
+

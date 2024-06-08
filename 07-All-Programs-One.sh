77e585b5b327831bad300c967bcab2ee9dc1ce21
unknown <chandrasekhar.maroju@gmail.com>
2024-06-08 07:29:28 +0530

    added 7th script-practice

--- 07-All-Programs-One.sh
+++ 07-All-Programs-One.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash    
 
 echo "1.This is My practice program to Check all commands" 
 
-echo -e "\n2.Enter your Name : "
+echo -e "\n 2.Enter your Name : "
 read Name
 
-echo "3.I entered my name in Input as:" 
+echo "3.I entered my name in input as:" 
 echo $Name
 

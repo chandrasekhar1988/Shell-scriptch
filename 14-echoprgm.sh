b3026666730cf9dbed16a5652aeebfb8350f3fd3
unknown <chandrasekhar.maroju@gmail.com>
2024-06-08 14:21:06 +0530

    added 7th script-practice

--- 14-echoprgm.sh
+++ 14-echoprgm.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo "Hello shell script"
+
 #Echo With New Line
 echo -e 'firstline\nsecondline\nthirdline\nfourthline'
+
+
+
--- 17-arraysprgm.sh
+++ 17-arraysprgm.sh
@@ -5,11 +5,13 @@ arr=(24 27 84 11 99)
 
 #we only assign value to a specific index.
 arr[5]=78
+
 # To print all elements of the array
 echo "Given array: ${arr[*]}"
-echo “Printing Array again${arr[@]}”
+echo "Printing Array again : ${arr[@]}"
+
 
 # To print the first element
-echo “The first element:”
-echo “${arr[0]}”
+echo "The first element: "
+echo "${arr[0]}"
 

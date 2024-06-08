7f402aed52edc1c9068c584f07e01d3717259583
unknown <chandrasekhar.maroju@gmail.com>
2024-06-08 11:24:54 +0530

    added 7th script-practice

--- 11-misc-prgms.sh
+++ 11-misc-prgms.sh
@@ -3,6 +3,10 @@
 #echo $((RANDOM))
 #Performing Mathematical Operations Without Storing
 
+
+#Echo With New Line
+echo -e 'firstline\nsecondline\nthirdline\nfourthline'
+
 read -p "Enter a number:" num1
 read -p "Enter a smaller number:" num2
 
@@ -10,3 +14,5 @@ echo "Addition: $(($num1 + $num2))"
 echo "Subtraction: $(($num1 - $num2))"
 echo "Multiplication: $(($num1 * $num2))"
 echo "Division: $(($num1 / $num2))"
+
+
---
+++ 14-echoprgm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Echo With New Line
+echo -e 'firstline\nsecondline\nthirdline\nfourthline'

fd5f64edb6c29f2b47858c77068d3bc83872d940
unknown <chandrasekhar.maroju@gmail.com>
2024-06-09 08:49:20 +0530

    Comments

--- 06-arraysprgm.sh
+++ 06-arraysprgm.sh
@@ -2,16 +2,24 @@
 #we declare the array along with the values.
 arr=(24 27 84 11 99)
 
-
 #we only assign value to a specific index.
 arr[5]=78
 
 # To print all elements of the array
 echo "Given array: ${arr[*]}"
-echo "Printing Array again : ${arr[@]}"
-
+echo "Printing Array again : ${arr[@]}" #both same
 
 # To print the first element
 echo "The first element: "
 echo "${arr[0]}"
 
+#array with strings
+FRUITS=("Apple" "Banana" "Mango")
+echo "First Value is : ${FRUITS[0]}"
+echo "Second Value is : ${FRUITS[1]}"
+echo "Third Value is : ${FRUITS[2]}"
+
+
+
+
+ 

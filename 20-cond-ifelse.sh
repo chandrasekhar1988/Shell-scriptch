879314a8dcb59bb22aa4e188062e3d0c78aaa1c4
unknown <chandrasekhar.maroju@gmail.com>
2024-06-09 07:54:54 +0530

    Comments

---
+++ 20-cond-ifelse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash    
+#if.else example
+#Initializing two variables 
+a=20 
+b=20 
+  
+if [ $a == $b ] 
+then 
+    #If they are equal then print this 
+    echo "a is equal to b"
+else
+    #else print this 
+    echo "a is not equal to b"
+fi
+
+

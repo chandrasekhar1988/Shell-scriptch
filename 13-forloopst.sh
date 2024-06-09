9bd35cd31c0b386b93d28e884c3fffc5806e93ec
unknown <chandrasekhar.maroju@gmail.com>
2024-06-09 08:26:12 +0530

    Comments

--- 13-forloopst.sh
+++ 13-forloopst.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#For Loop : Print Numbers From 1 to 10 
-for (( i=1; i<=10; i++ ))
+#For Loop : Print Numbers From 1 to 4 
+for (( i=1; i<=4; i++ ))
 do
 echo $i
 done
 
-#For Loop : Print Even Numbers From 1 to 10 
-for (( j=1; j<=10; j++ ))
+#For Loop : Print Even Numbers From 1 to 5 
+for (( j=1; j<=5; j++ ))
 do
 if [ $((j%2)) == 0 ]
 then
@@ -14,13 +14,3 @@ echo $j
 fi
 done
 
-for k in 1 2 3 4 5
-do
-echo "Hello $k"
-done
-
-arr=("mango" "grape" "apple" "cherry" "orange")
-for item in "${arr[@]}";
-do
-echo $item
-done
--- 15-forloop.sh
+++ 15-forloop.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#For Loop : Print Even Numbers From 1 to 10 
-# for (( i=1; i<=10; i++ ))
-# do
-# #if [ $((i%2)) == 0 ]
-# #then
-# echo $i
-# #fi
-# done
+for k in 1 2 3 4 5
+do
+echo "Hello $k"
+done
 
 arr=("mango" "grape" "apple" "cherry" "orange")
-for item in "${arr[@]}"; do
+for item in "${arr[@]}";
+do
 echo $item
 done

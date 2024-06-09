ff38f5d22ab71f06a4ac604961e6f1f50fed310f
unknown <chandrasekhar.maroju@gmail.com>
2024-06-09 08:06:50 +0530

    Comments

---
+++ 12-switchA-prgm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#switch statement example
+CARS="bmw"
+  
+#Pass the variable in string 
+case "$CARS" in 
+    #case 1 
+    "mercedes") echo "Headquarters - Affalterbach, Germany" ;; 
+      
+    #case 2 
+    "audi") echo "Headquarters - Ingolstadt, Germany" ;; 
+      
+    #case 3 
+    "bmw") echo "Headquarters - Chennai, Tamil Nadu, India" ;; 
+esac 
+
+

fda742ded350a77937a5f0f531d246a15a62c94b
unknown <chandrasekhar.maroju@gmail.com>
2024-06-10 07:22:12 +0530

    Comments

--- 01-install-mysql.sh
+++ 01-install-mysql.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-
 ID=$(id -u)
 if [ $ID -ne 0 ]
 then
 echo "ERROR: Please run this script with root access"
 exit 1
 # $?-you have to give other than 0
-
 else
 echo "you are root user"
 fi
 
 yum install mysql -y
+#To confirm/check whether mysql is installed or not
+if [ $? -ne 0 ]
+then
+echo "ERROR: Installing MYSQL is failed"
+exit 1  # $?-you have to give other than 0
+else
+echo "Installing MYSQL is success"
+fi
+
 
 
 

514d2cad848388f908bf6034588590f04703ab7e
unknown <chandrasekhar.maroju@gmail.com>
2024-06-10 19:15:40 +0530

    Comments

--- 03.misc-colors.sh
+++ 03.misc-colors.sh
@@ -2,7 +2,9 @@
 
 RED="\e[31m"
 GREEN="\e[32m"
+BOLDGREEN="\e[1;${GREEN}m"
+ITALICRED="\e[3;${RED}m"
 ENDCOLOR="\e[0m"
 
 echo -e "${RED}Hello Chandra, ${ENDCOLOR}Wow"
-echo -e "${GREEN}Welcome to my world ${ENDCOLOR}Enjoy"
+echo -e "${BOLDGREEN}Welcome to my world ${ITALICRED}Enjoy"

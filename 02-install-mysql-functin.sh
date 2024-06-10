b05655ee6266549e188ce61537d09f55bc23b7af
unknown <chandrasekhar.maroju@gmail.com>
2024-06-10 08:01:51 +0530

    Comments

--- 02-install-mysql-functin.sh
+++ 02-install-mysql-functin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ID=$(id -u)
 VALIDATE(){
-if [ $? -ne 0 ] 
+if [ $1 -ne 0 ] 
 then
-echo "ERROR: Installing is failed"
+echo "ERROR: $2..... is failed"
 exit 1  
 else
-echo "Installing is success"
+echo "$2..... is success"
 fi
 }
 if [ $ID -ne 0 ] 
@@ -17,9 +17,10 @@ else
 echo "you are root user"
 fi
 yum install mysql -y
-VALIDATE
+VALIDATE $? "Installing MySQL!wow"
 yum install git -y
-VALIDATE
+VALIDATE $? "Installing git!wow"
+
 
 
 

b91c4f24b3ddf3b774eae07ccd862c3850b58fe5
unknown <chandrasekhar.maroju@gmail.com>
2024-06-10 07:02:50 +0530

    Comments

--- 01-install-mysql.sh
+++ 01-install-mysql.sh
@@ -4,10 +4,13 @@ ID=$(id -u)
 if [$ID -ne 0]
 then
 echo "ERROR: Please run this script with root access"
+exit 1 # $?-you have to give other than 0
+
 else
 echo "you are root user"
 fi
 
 yum install mysql -y
+
 
 

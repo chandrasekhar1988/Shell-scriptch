506250ea18d1c40069ff8ff82103f7c2216fc01e
unknown <chandrasekhar.maroju@gmail.com>
2024-06-11 07:00:41 +0530

    Comments

--- 02.logsprgm.sh
+++ 02.logsprgm.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 ID=$(id -u)
-#Checking Logsfile for execution status/logs
+#Date, Colour format program
+
 TIMESTAMP=$(date +%F-%H-%M-%S)
+echo "1.for printing dates in specified format---> $TIMESTAMP "
+
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
 VALIDATE(){
 if [ $1 -ne 0 ] 
 then
@@ -27,6 +31,9 @@ yum install mysql -y &>> $LOGFILE
 VALIDATE $? "Installing MySQL!wow"
 yum install git -y &>> $LOGFILE
 VALIDATE $? "Installing git!wow"
+
+
+
 
 
 

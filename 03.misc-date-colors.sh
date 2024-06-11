85b76df3663e92b9472a2999110aec66fe47d856
unknown <chandrasekhar.maroju@gmail.com>
2024-06-11 06:38:03 +0530

    Comments

--- 03.misc-date-colors.sh
+++ 03.misc-date-colors.sh
@@ -6,7 +6,7 @@ echo "1.for printing dates in YYYY-MM-DD format---> $today "
 todayA=$(date +%y-%m-%d)
 echo "2.for printing dates in YY-MM-DD format---> $todayA "
 
-todayB=$(date “+%A, %B-%d, %Y”)
+todayB=$(date “+%A, %B, %Y”)
 echo "3.for printing dates in Weekday, Month Day, Year Format---> $todayB "
 
 c_time=$(date +%H:%M:%S)  

f727cb3e32f5375931648ceb734ac5c49328875b
unknown <chandrasekhar.maroju@gmail.com>
2024-06-11 06:53:22 +0530

    Comments

--- 03.misc-date-colors.sh
+++ 03.misc-date-colors.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-today=$(date +%Y-%m-%d)
-echo "1.for printing dates in YYYY-MM-DD format---> $today "
+# today=$(date +%Y-%m-%d)
+# echo "1.for printing dates in YYYY-MM-DD format---> $today "
 
-todaya=$(date +%y-%m-%d)
-echo "2.for printing dates in YY-MM-DD format---> $todaya "
+# todaya=$(date +%y-%m-%d)
+# echo "2.for printing dates in YY-MM-DD format---> $todaya "
 
-todayb=$(date “+%A, %B, %Y”)
-echo "3.for printing dates in Weekday, Month Day, Year Format---> $todayb "
+# todayb=$(date “+%A, %B, %Y”)
+# echo "3.for printing dates in Weekday, Month Day, Year Format---> $todayb "
 
-c_time=$(date +%H:%M:%S)  
-echo "4.for printing time in 24-hour format---> $c_time"
+# c_time=$(date +%H:%M:%S)  
+# echo "4.for printing time in 24-hour format---> $c_time"
 
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
 
+echo -e "${RED}This is some red text, ${ENDCOLOR}"
+echo -e "${GREEN}And this is some green text${ENDCOLOR}"
 
 

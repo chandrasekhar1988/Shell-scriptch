c2be13033680742ed7fa27fc9955de29e6e1ea47
unknown <chandrasekhar.maroju@gmail.com>
2024-06-11 06:51:28 +0530

    Comments

--- 02.logsprgm.sh
+++ 02.logsprgm.sh
@@ -2,23 +2,23 @@
 ID=$(id -u)
 #Checking Logsfile for execution status/logs
 TIMESTAMP=$(date +%F-%H-%M-%S)
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
 if [ $1 -ne 0 ] 
 then
-echo "ERROR: $2..... $R is failed $N"
+echo "ERROR: $2..... ${RED} is failed ${ENDCOLOR}"
 exit 1  
 else
-echo "$2..... $G is success $N"
+echo "$2..... ${GREEN} is success ${ENDCOLOR}"
 fi
 }
 if [ $ID -ne 0 ] 
 then 
-echo "$R ERROR: Please run this script with root access $N"
+echo "${RED} ERROR: Please run this script with root access ${ENDCOLOR}"
 exit 1
 else
 echo "you are root user"
